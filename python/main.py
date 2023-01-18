4667208bc5a59cc2b9f5813139f1781ffafd8aaf
maxmarsc <maxime.coutant@grenoble-inp.org>
2023-01-18 10:48:35 +0100

    Added 2x oversampling

--- python/main.py
+++ python/main.py
@@ -7,6 +7,7 @@ from scipy.signal import welch
 # import sys
 import matplotlib.pyplot as plt
 import matplotlib
+import soxr
 
 WAVEFORM_LEN = 2048
 SAMPLERATE = 44100
@@ -20,10 +21,32 @@ BUTTER2_COEFFS = np.array([
 ])
 
 BUTTER4_COEFFS = np.array([
-    [-1.4479 + 0.5998j, -1.4479 - 0.5998j, 1.4479 - 3.4956j, 1.4479 + 3.4956j],  # residuals
-    [-1.1995 + 2.8959j, -1.1995 - 2.8959j, -2.8959 + 1.1995j, -2.8959 - 1.1995j] # poles
+    [1.306104 - 3.153214j, 1.306104 + 3.153214j, -1.306104 + 0.541006j, -1.306104 - 0.541006j],  # residuals
+    [-2.612208 + 1.082012j, -2.612208 - 1.082012j, -1.082012 + 2.612208j, -1.082012 - 2.612208j] # poles
 ])
 
+BUTTERWORTH_COEFFS = {
+    44100 : {
+        2 : [
+            [0.0000 - 1.999274j, 0.0000 + 1.999274j],                   # residuals
+            [-1.999274 + 1.999274j, -1.999274 - 1.999274j]             # poles
+        ],
+        4 : [
+            [1.306104 - 3.153214j, 1.306104 + 3.153214j, -1.306104 + 0.541006j, -1.306104 - 0.541006j],  # residuals
+            [-2.612208 + 1.082012j, -2.612208 - 1.082012j, -1.082012 + 2.612208j, -1.082012 - 2.612208j] # poles
+        ]
+    },
+    88200 : {
+        2 : [
+            [0.000000 -0.999649j, 0.000000 +0.999649j],         #r
+            [-0.999649 +0.999649j, 0.999649 -0.999649j]         #p
+        ],
+        4 : [
+            [0.653052 -1.576607j, 0.653052 +1.576607j, -0.653052 +0.270503j, -0.653052 -0.270503j],      #r
+            [-1.306104 +0.541006j, -1.306104 -0.541006j, -0.541006 +1.306104j, -0.541006 -1.306104j]         #p
+        ]
+    }
+}
 
 # NAIVE_SAW = np.zeros(WAVEFORM_LEN)
 # NAIVE_SAW[:WAVEFORM_LEN//2] = np.linspace(0, 1, WAVEFORM_LEN//2, endpoint=True)
@@ -129,12 +152,13 @@ def process_naive_linear(waveform, x_values):
         relative_idx = x_red * waveform_len
 
         prev_idx = floor(relative_idx)
+        next_idx = (prev_idx + 1) % waveform_len
 
         if relative_idx == prev_idx:
             y[i] = waveform[prev_idx]
         else:
-            a = (waveform[prev_idx + 1] - waveform[prev_idx]) / (x - prev_x)
-            b = (waveform[prev_idx +1] * (x - prev_x) - prev_x * (waveform[prev_idx + 1] - waveform[prev_idx])) / (x - prev_x)
+            a = (waveform[next_idx] - waveform[prev_idx]) / (next_idx - prev_x)
+            b = (waveform[next_idx] * (next_idx - prev_x) - prev_x * (waveform[next_idx] - waveform[prev_idx])) / (next_idx - prev_x)
             y[i] = a * x + b
 
         prev_x = x
@@ -145,20 +169,21 @@ def process_naive_linear(waveform, x_values):
 
 
 def main():
-    orders = len(BUTTER2_COEFFS[0])
     play_freq = 1000
 
     num_frames_total = int(DURATION_S * SAMPLERATE)
+    num_frames_total_up = int(DURATION_S * SAMPLERATE * 2)
+
     x = np.linspace(0.0, num_frames_total*play_freq / SAMPLERATE, num_frames_total + 1, endpoint=True)[1:]
-    y = np.zeros(num_frames_total)
+    x_up = np.linspace(0.0, num_frames_total*play_freq / SAMPLERATE, num_frames_total_up + 1, endpoint=True)[1:]
+    y2 = np.zeros(num_frames_total)
     y4 = np.zeros(num_frames_total)
+    y2_up = np.zeros(num_frames_total_up)
+    y4_up = np.zeros(num_frames_total_up)
+
+
+    # Precomputing m & q diffs
     (m, q) =  compute_m_q_vectors(NAIVE_SAW, NAIVE_SAW_X)
-
-    
-    # print("x {} {} {} : {} {} {}".format(x[-3], x[-2], x[-1], x[0], x[1], x[2]))
-    # print("X {} {} {} : {} {} {}".format(NAIVE_SAW_X[-3], NAIVE_SAW_X[-2], NAIVE_SAW_X[-1], NAIVE_SAW_X[0], NAIVE_SAW_X[1], NAIVE_SAW_X[2]))
-
-    # Precomputing diffs
     m_diff = np.zeros(m.shape[0])
     q_diff = np.zeros(q.shape[0])
     
@@ -170,43 +195,57 @@ def main():
 
     abc = None
 
-    for order in range(0, orders, 2):
-        ri = BUTTER2_COEFFS[0][order]
-        zi = BUTTER2_COEFFS[1][order]
-        y += process_fwd(x, ri, zi, NAIVE_SAW_X, m, q, m_diff, q_diff)
-        # y += process(x, ri, zi, NAIVE_SAW_X, m, q, m_diff, q_diff)
+    for order in range(0, 2, 2):
+        ri = BUTTERWORTH_COEFFS[44100][2][0][order]
+        zi = BUTTERWORTH_COEFFS[44100][2][1][order]
+        y2 += process_fwd(x, ri, zi, NAIVE_SAW_X, m, q, m_diff, q_diff)
+
+        ri_up = BUTTERWORTH_COEFFS[88200][2][0][order]
+        zi_up = BUTTERWORTH_COEFFS[88200][2][1][order]
+        y2_up += process_fwd(x_up, ri_up, zi_up, NAIVE_SAW_X, m, q, m_diff, q_diff)
     
-    # for order in range(0, 4, 2):
-    #     ri = BUTTER4_COEFFS[0][order]
-    #     zi = BUTTER4_COEFFS[1][order]
-    #     (new_y, _) = process(x, ri, zi, NAIVE_SAW_X, m, q, m_diff, q_diff)
+    for order in range(0, 4, 2):
+        ri = BUTTERWORTH_COEFFS[44100][4][0][order]
+        zi = BUTTERWORTH_COEFFS[44100][4][1][order]
+        y4 += process_fwd(x, ri, zi, NAIVE_SAW_X, m, q, m_diff, q_diff)
 
-    #     y4 += new_y
+        ri_up = BUTTERWORTH_COEFFS[88200][4][0][order]
+        zi_up = BUTTERWORTH_COEFFS[88200][4][1][order]
+        y4_up += process_fwd(x_up, ri_up, zi_up, NAIVE_SAW_X, m, q, m_diff, q_diff)
 
-    # y_naive = process_naive_linear(NAIVE_SAW, x)
+    y_naive = process_naive_linear(NAIVE_SAW, x)
 
 
-    # freqs, powers = welch(y, fs=SAMPLERATE)
-    # freqs_naive, power_naive = welch(y_naive, fs=SAMPLERATE)
-    fig, axs = plt.subplots(3)
-    # axs[0].loglog(freqs, powers, 'b')
-    # axs[0].hist(np.log2(powers), log=True, bins=freqs)
-    axs[1].psd(y, Fs=SAMPLERATE, NFFT=4096, color="b", label="ADAA-butterworth-2")
-    # axs[0].psd(y4, Fs=SAMPLERATE, NFFT=2048, color="black", label="ADAA-butterworth-2")
-    # axs[0].psd(y_naive, Fs=SAMPLERATE, NFFT=2048, color="r", label="naive-linear")
+    # Downsample the upsampled output
+    y2_ds = soxr.resample(
+        y2_up,
+        88200,
+        44100
+    )
+    y4_ds = soxr.resample(
+        y4_up,
+        88200,
+        44100
+    )
 
-    # axs[0].loglog(freqs_naive, power_naive, 'r')
-    # axs[1].hist(np.log2(power_naive), log=True, bins=freqs_naive)
-    # test = np.zeros(num_frames_total)
-    # test[440] = 1.0
-    # for i in (1, 45, 89, 133, 177):
-    #     test[i] = -1.
+    fig, axs = plt.subplots(5)
 
-    # axs[2].plot(x, y_naive, 'r', label="naive-linear")
-    axs[2].plot(x, y, 'b', label="ADAA-butterworth-2")
-    # axs[2].plot(x, y4, 'black', label="ADAA-butterworth-4")
-    # axs[2].plot(x, abc, 'g')
-    # axs[2].plot(x, test, 'purple')
+    axs[0].psd(y2[3:], Fs=SAMPLERATE, NFFT=4096, color="b", label="ADAA-butterworth-2")
+    axs[1].psd(y4[3:], Fs=SAMPLERATE, NFFT=4096, color="black", label="ADAA-butterworth-2")
+    axs[2].psd(y2_ds[3:], Fs=SAMPLERATE, NFFT=4096, color="green", label="ADAA-butterworth-2-OVS")
+    axs[3].psd(y4_ds[3:], Fs=SAMPLERATE, NFFT=4096, color="purple", label="ADAA-butterworth-4-OVS")
+
+    for ax_idx in (0, 1, 2, 3):
+        axs[ax_idx].minorticks_on()
+        axs[ax_idx].grid(which="minor", linestyle=":")
+
+    axs[4].plot(x, y_naive, 'r', label="naive-linear")
+    axs[4].plot(x, y2, 'b', label="ADAA-butterworth-2")
+    axs[4].plot(x, y4, 'black', label="ADAA-butterworth-4")
+    axs[4].plot(x, y2_ds, 'green', label="ADAA-butterworth-2-OVS")
+    axs[4].plot(x, y2_ds, 'purple', label="ADAA-butterworth-4-OVS")
+
+
     plt.grid(True, which="both")
     plt.legend()
     plt.show()
